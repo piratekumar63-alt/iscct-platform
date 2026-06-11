ca72581168cb2d38222a242fae5f71accf05cc4a
piratekumar63-alt <piratekumar63@gmail.com>
2026-06-12 03:47:13 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('<h1>ISCCT Website Coming Soon</h1>');
+});
+
+server.listen(process.env.PORT || 3000);
