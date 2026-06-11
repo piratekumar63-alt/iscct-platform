cf52b0a903ce6e44d168a6db6e63178278f7f0cf
piratekumar63-alt <piratekumar63@gmail.com>
2026-06-12 04:10:08 +0530

    server.js

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('<h1>ISCCT Website Coming Soon</h1>');
+  fs.readFile('index.html', (err, data) => {
+    if (err) {
+      res.writeHead(500);
+      res.end('Error loading page');
+      return;
+    }
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(data);
+  });
 });
 
-server.listen(process.env.PORT || 3000);
+server.listen(process.env.PORT || 8080);
